6cdcaf23d8c44475be76f1d0928bed0b612c7528
Mike Verdicchio <mikeverdicchio@gmail.com>
2022-08-28 23:38:13 -0400

    Setup project with docker-compose, node files, and eslint

---
+++ README.md
@@ -0,0 +1,21 @@
+# Trino Proxy
+
+A load-balancer and gateway service for Trino.
+
+## Running Locally
+
+This project requires Node16+, Docker, and docker-compose. Please ensure you have these tools installed and updated.
+
+Build and run docker containers:
+
+```sh
+make start
+```
+
+## Contributing
+
+Format and lint the source code:
+
+```sh
+make format
+```
--- src/server.js
+++ src/server.js
@@ -1,114 +1,129 @@
-const { createClient } = require('redis');
-const express = require('express')
-const app = express()
-const port = 5111
-const presto_url = 'http://localhost:5110'
-const axios = require('axios').default;
-const uuidv4 = require('uuid').v4;
+const { createClient } = require("redis");
+const express = require("express");
+const axios = require("axios").default;
+const uuidv4 = require("uuid").v4;
 
-
-const client = createClient();
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-
-app.use(express.json()) // for parsing application/json
-app.use(express.text()) // for parsing application/json
-
-function updateUrls(body, newQueryId){
-    const queryId = body.id;
-    body.id = newQueryId;
-
-
-    if(body.infoUri)
-        body.infoUri = body.infoUri.replace(queryId, newQueryId);
-    if (body.nextUri)
-        body.nextUri = body.nextUri.replace(queryId, newQueryId);
-    return body;
+const { REDIS_URL } = process.env;
+if (!REDIS_URL) {
+  throw new Error("REDIS_URL not set");
 }
 
-app.post('/v1/statement', async (req, res) => {
-    console.log(req.headers)
-    console.log(req.body)
+const client = createClient({ url: REDIS_URL });
+client.on("error", (err) => console.log("Redis Client Error", err));
 
+const app = express();
+const port = 8080;
+const presto_url = "http://localhost:5110";
 
-    axios({
-        url: presto_url+'/v1/statement', 
-        method: 'post',
-        headers: req.headers,
-        data: req.body})
-      .then(async function (response) {
+app.use(express.json()); // for parsing application/json
+app.use(express.text()); // for parsing application/json
 
-        const queryId = response.data.id;
-        const newQueryId = uuidv4();
-        const newBody = updateUrls(response.data, newQueryId)
+function updateUrls(body, newQueryId) {
+  const queryId = body.id;
+  body.id = newQueryId;
 
-        await client.set(newQueryId, queryId, {
-            EX: 60*60,
-           // NX: true
-          });
+  if (body.infoUri) {
+    body.infoUri = body.infoUri.replace(queryId, newQueryId);
+  }
+  if (body.nextUri) {
+    body.nextUri = body.nextUri.replace(queryId, newQueryId);
+  }
+  return body;
+}
 
-        //console.log(response.data);
-       // process.exit(1)
+app.post("/v1/statement", async (req, res) => {
+  console.log(req.headers);
+  console.log(req.body);
 
-        res.json(response.data);
-      });
-})
+  axios({
+    url: presto_url + "/v1/statement",
+    method: "post",
+    headers: req.headers,
+    data: req.body,
+  }).then(async function (response) {
+    const queryId = response.data.id;
+    const newQueryId = uuidv4();
 
-app.get('/v1/statement/queued/:queryId/:keyId/:num', async (req, res) => {
-   // console.log("Trouble");
-   const newQueryId = await client.get(req.params.queryId);
-   console.log("1Trouble", req.params.queryId);
+    await client.set(newQueryId, queryId, {
+      EX: 60 * 60,
+      // NX: true
+    });
 
-   console.log("2Trouble", newQueryId);
-   if(!newQueryId)
-   process.exit(1)
-    axios({
-        url: presto_url+'/v1/statement/queued/'+newQueryId+'/'+req.params.keyId+'/'+req.params.num, 
-        method: 'get',
-        headers: req.headers})
-      .then(function (response) {
-    const newBody = updateUrls(response.data, req.params.queryId)
+    //console.log(response.data);
+    // process.exit(1)
 
-        res.json(newBody);
-      });
-})
+    res.json(response.data);
+  });
+});
 
-app.get('/v1/statement/executing/:queryId/:keyId/:num', async (req, res) => {
-    const newQueryId = await client.get(req.params.queryId);
-    console.log("1Trouble", req.params.queryId);
+app.get("/v1/statement/queued/:queryId/:keyId/:num", async (req, res) => {
+  // console.log("Trouble");
+  const newQueryId = await client.get(req.params.queryId);
+  console.log("1Trouble", req.params.queryId);
 
-    console.log("2Trouble", newQueryId);
-    if(!newQueryId){
-      process.exit(1)
-      // didn't find query id
-    }
-    axios({
-        url: presto_url+'/v1/statement/executing/'+newQueryId+'/'+req.params.keyId+'/'+req.params.num, 
-        method: 'get',
-        headers: req.headers})
-      .then(function (response) {
-       const newBody = updateUrls(response.data, req.params.queryId)
-       res.json(newBody);
-      });
-})
+  console.log("2Trouble", newQueryId);
+  if (!newQueryId) process.exit(1);
+  axios({
+    url:
+      presto_url +
+      "/v1/statement/queued/" +
+      newQueryId +
+      "/" +
+      req.params.keyId +
+      "/" +
+      req.params.num,
+    method: "get",
+    headers: req.headers,
+  }).then(function (response) {
+    const newBody = updateUrls(response.data, req.params.queryId);
 
-app.use((req, res, next) => {
-    console.log(req.method)
+    res.json(newBody);
+  });
+});
 
-    console.log(req.path)
-    console.log(req.headers)
-    console.log(req.text)
+app.get("/v1/statement/executing/:queryId/:keyId/:num", async (req, res) => {
+  const newQueryId = await client.get(req.params.queryId);
+  console.log("1Trouble", req.params.queryId);
 
-    console.log(req.body)
-    res.send('Hello World!')
-  })
- 
-async function main(){
+  console.log("2Trouble", newQueryId);
+  if (!newQueryId) {
+    process.exit(1);
+    // didn't find query id
+  }
+  axios({
+    url:
+      presto_url +
+      "/v1/statement/executing/" +
+      newQueryId +
+      "/" +
+      req.params.keyId +
+      "/" +
+      req.params.num,
+    method: "get",
+    headers: req.headers,
+  }).then(function (response) {
+    const newBody = updateUrls(response.data, req.params.queryId);
+    res.json(newBody);
+  });
+});
+
+app.use((req, res) => {
+  console.log(req.method);
+
+  console.log(req.path);
+  console.log(req.headers);
+  console.log(req.text);
+
+  console.log(req.body);
+  res.send("Hello World!");
+});
+
+async function main() {
   await client.connect();
 
   app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-  })
+    console.log(`Example app listening on port ${port}`);
+  });
 }
+
 main();
