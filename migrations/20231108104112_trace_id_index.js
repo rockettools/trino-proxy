2356956a370a7628f382e2c8090be62b95eb1917
Mike Verdicchio <mikeverdicchio@gmail.com>
2023-11-08 06:14:32 -0500

    Add index on trace_id to improve assumed user queries

---
+++ migrations/20231108104112_trace_id_index.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(
+    "CREATE INDEX IF NOT EXISTS trace_id_index ON query USING btree (trace_id)"
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw("DROP INDEX IF EXISTS trace_id_index ON query");
+};
