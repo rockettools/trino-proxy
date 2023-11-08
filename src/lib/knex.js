e4748dca342a045d6a78b376a061d418c3128b09
Mike Verdicchio <mikeverdicchio@gmail.com>
2023-11-08 06:14:04 -0500

    Adjust knex pool and timeout settings

--- src/lib/knex.js
+++ src/lib/knex.js
@@ -6,4 +6,13 @@ if (!connectionString) {
 exports.knex = require("knex")({
   client: "pg",
   connection: connectionString,
+  /** It is recommended to set min to 0 so idle connections can be terminated */
+  /** @see https://knexjs.org/guide/#pool */
+  pool: {
+    min: 0,
+    max: 10,
+  },
+  /** How long knex waits before throwing a timeout error while acquiring a connection */
+  /** @see https://knexjs.org/guide/#acquireconnectiontimeout */
+  acquireConnectionTimeout: 10000,
 });
