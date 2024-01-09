8d26c21b5a091b3f379c4dfbfa30dc911380b72c
Mike Verdicchio <mikeverdicchio@gmail.com>
2024-01-09 10:23:35 -0500

    Add index on query.created_at for improved performance on query analysis

---
+++ migrations/20240109150009_query_created_at_index.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(
+    "CREATE INDEX IF NOT EXISTS created_at_index ON query USING btree (created_at DESC)"
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw("DROP INDEX IF EXISTS created_at_index ON query");
+};
